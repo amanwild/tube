117ef9733734b6d989c9afedd60867a16f753117
Aman Sahu <amansahu1130@gmail.com>
2022-09-26 11:14:49 +0530

    both are combined in my-tube

--- my-tube.py
+++ my-tube.py
@@ -1,6 +1,7 @@
+from pytube import Playlist
 from moviepy.editor import *
 import os.path
-
+import re
 from os.path import exists
 from random import randrange
 from pathlib import Path
@@ -13,115 +14,164 @@ from pytube import YouTube
 import random
 import string
 
-link = "https://www.youtube.com/watch?v=n5vLzoLEUhc"
-youtube = YouTube(link)
-Title =(youtube.title).replace('"','')
-Title =Title.replace('|','')
-print("\n Title : ", Title)
+link = "https://www.youtube.com/watch?v=iSumr2i9BvQ"
+# link = "https://www.youtube.com/watch?v=Ai50j-9Ga7w&list=PLOZsCzXAYggsP7uRg3ZezVP7xR88plP7L"
 
 
-# videos =youtube.streams.filter(only_video=True)#it give all formates
-videos = youtube.streams.all()  # it give all formates
-vid = list(enumerate(videos))
-reslist = []#For Storing All Available resulation
-a = 0
-def delete(a,v):
-    os.remove(a)                             
-    os.remove(v)                             
-    print('Mixing Done')
-    
-for i in vid:
-    print(i)
-    
-for i in vid:
-    # Geting all metadata form vid
-    tupl = list(i)
-    strtupl = str(tupl[1])
-    metalist = list(strtupl.split())
+print("Welcome to MyTube")
+try:
+  youtube = Playlist(link)
+  print(f'Playlist: {youtube.title}')
+  length =len(youtube.video_urls)
+  print(f'Playlist length: {length}')
+  print(0)
 
-    tyvi = 'type="video">'
-    if (metalist[-1] == tyvi):
-        res = ''.join(filter(lambda i: i.isdigit(), metalist[3]))
-        if res!="":
-            reslist.append(int(res))
-        
-print(reslist, " Full Resulation list (After append)")
-# maxx = reslist.index(max( int(i) for i in reslist))
+  vid = list(enumerate(youtube.videos))
+  for vid in youtube.videos:
+      Title =(vid.title).replace('"','')
+      Title = re.sub('[^a-zA-Z.\d\s]', '', Title)
+      print("\n Video name : ", Title)
+      
+      reslist = []#For Storing All Available resulation
+      a = 0
+      def delete(a,v):
+          os.remove(a)                             
+          os.remove(v)                             
+          print('Mixing Done')
+      videos = vid.streams.all()
+      vid = list(enumerate(videos))
+    #   for i in vid:
+    #       print(i,"  iiiiiiiiiiiiiiii")
+      for i in vid:
+          tupl = list(i)
+          strtupl = str(tupl[1])
+          metalist = list(strtupl.split())
+          tyvi = 'type="video">'
+          if (metalist[-1] == tyvi):
+              res = ''.join(filter(lambda i: i.isdigit(), metalist[3]))
+              if res!="":
+                  reslist.append(int(res))
+      print(reslist, " Full Resulation list (After append)")
+      max_value = max(reslist)
+      # index of max resulation
+      max_indes = reslist.index(max_value)
+      indices = [index for index, value in enumerate(reslist) if value == max_value]
+      print(indices, "All max indices")
+      maxx = max(indices)
+      print(maxx, "Selected max indice for video download")
+      
+      src =r"D:/vdo from tube/"
+      audio_index = (videos[len(vid)-1])
+      ado = src+audio_index.default_filename
+      upado = str(src+"a "+audio_index.default_filename)
+      video_index = (videos[maxx])
+      vdo = src+video_index.default_filename
+      upvdo = str(src+"v "+video_index.default_filename)
+      
+      #audio path for cmd line
+      asrc = str(PureWindowsPath(f"D:\\vdo from tube\\"+"a "+audio_index.default_filename))
+      #Video path for cmd line
+      vsrc = str(PureWindowsPath(f"D:\\vdo from tube\\"+"v "+video_index.default_filename))
+      #OUTput path for cmd line
+      osrc = (src+Title+".mp4")
+      
+      print(os.path.isfile(osrc))
+      i=1
+      print(osrc," :::::::::::Before name")
+      while (os.path.exists(osrc)):
+          osrc = src+Title+" MyTube_"+str(i)+" .mp4";
+          i += 1;
+      print(osrc,"  :::::::::::updated name")
+      # print("Before file Audio :: "+str(ado)+"\n"+
+      #     "Before file Video :: "+str(vdo)+"\n"+
+      #     "Created file Audio :: "+str(upado)+"\n"+
+      #     "Created file Video :: "+str(upvdo)+"\n"+
+      #     "Src for A          :: "+str(asrc)+"\n"+
+      #     "Src for V          :: "+str(vsrc)+"\n"+
+      #     "Output Path        :: "+str(osrc)+"\n")
+      
+      audio_index.download(src)
+      os.rename(ado, upado)
+      print("\n Audio Successfully Download \n")
+      videos[maxx].download(src)
+      os.rename(vdo,upvdo)
+      print("\n Video Successfully Download \n")
+      
+      os.system(f'ffmpeg -i "{vsrc}" -i "{asrc}" -c copy "{osrc}"')  
+      delete(asrc,vsrc)
 
-# max resulation
-max_value = max(reslist)
-# index of max resulation
-max_indes = reslist.index(max_value)
-indices = [index for index, value in enumerate(reslist) if value == max_value]
-print(indices, "All max indices")
-maxx = max(indices)
-print(maxx, "Selected max indice for video download")
-code = ''.join((random.choice(string.ascii_lowercase) for x in range(3))) # run loop until the define length
-print(code)
+except:
+  youtube = YouTube(link)
+  videos = youtube.streams.all()  # it give all formates
+  vid = list(enumerate(videos))
+  
+  Title = re.sub('[^a-zA-Z.\d\s]', '', (youtube.title))
+  print("\n Video Title : ", Title)
+  reslist = []#For Storing All Available resulation
+  
+  def delete(a,v):
+      os.remove(a)                              
+      os.remove(v)                             
+  for i in vid:
+      # Geting all metadata form vid
+      tupl = list(i)
+      strtupl = str(tupl[1])
+      metalist = list(strtupl.split())
+      tyvi = 'type="video">'
+      if (metalist[-1] == tyvi):
+          res = ''.join(filter(lambda i: i.isdigit(), metalist[3]))
+          if res!="":
+              reslist.append(int(res))
+              
+  print(reslist, " Full Resulation list (After append)")
+  max_value = max(reslist)
+  # index of max resulation
+  max_indes = reslist.index(max_value)
+  indices = [index for index, value in enumerate(reslist) if value == max_value]
+  maxx = max(indices)
+  print(maxx, "Selected max indice for video download")
+  
+  src =r"D:/vdo from tube/"
+  audio_index = (videos[len(vid)-1])
+  ado = src+audio_index.default_filename
+  upado = str(src+"a "+audio_index.default_filename)
+  video_index = (videos[maxx])
+  vdo = src+video_index.default_filename
+  upvdo = str(src+"v "+video_index.default_filename)
+  
+  #audio path for cmd line
+  asrc = str(PureWindowsPath(f"D:\\vdo from tube\\"+"a "+audio_index.default_filename))
+  #Video path for cmd line
+  vsrc = str(PureWindowsPath(f"D:\\vdo from tube\\"+"v "+video_index.default_filename))
+  #OUTput path for cmd line
+  osrc = (src+Title+".mp4")
+  
+  print(os.path.isfile(osrc))
+  i=1
+  #if File Already Exist
+  while (os.path.exists(osrc)):
+      print(osrc," :::::::::::Before Updation")
+      osrc = src+Title+" MyTube_"+str(i)+" .mp4";
+      i += 1;
+      print(osrc,"  :::::::::::After Updation")
+  # print("Before file Audio :: "+str(ado)+"\n"+
+  #       "Before file Video :: "+str(vdo)+"\n"+
+  #       "Created file Audio :: "+str(upado)+"\n"+
+  #       "Created file Video :: "+str(upvdo)+"\n"+
+  #       "Src for A          :: "+str(asrc)+"\n"+
+  #       "Src for V          :: "+str(vsrc)+"\n"+
+  #       "Output Path        :: "+str(osrc)+"\n")
+  
+  audio_index.download(src)
+  os.rename(ado, upado)
+  print("\n Audio Successfully Download \n")
+  videos[maxx].download(src)
+  os.rename(vdo,upvdo)
+  print("\n Video Successfully Download \n")
+  
+  os.system(f'ffmpeg -i "{vsrc}" -i "{asrc}" -c copy "{osrc}"')  
+  delete(asrc,vsrc)
 
-src =r"D:/vdo from tube/"
-audio_index = (videos[len(vid)-1])
-ado = src+audio_index.default_filename
-upado = src+str(code)+" a "+audio_index.default_filename
-
-video_index = (videos[maxx])
-vdo = src+video_index.default_filename
-upvdo = src+str(code)+" v "+video_index.default_filename
-
-#audio path for cmd line
-asrc = PureWindowsPath(f"D:\\vdo from tube\\"+str(code)+" a "+audio_index.default_filename)
-#Video path for cmd line
-vsrc = PureWindowsPath(f"D:\\vdo from tube\\"+str(code)+" v "+video_index.default_filename)
-#OUTput path for cmd line
-osrc = (src+Title+".webm")
-
-print('D://vdo from tube//STRUGGLE HARD 😎🔥WhatsApp Status shorts Billionaire Attitude Status🔥motivation quotes.webm')
-
-print(os.path.isfile(osrc))
-i=1
-print(osrc," :::::::::::Before name")
-while (os.path.exists(osrc)):
-    print("\n Video Exist\n")
-    osrc = src+Title+str(i)+".mp4";
-    i += 1;
-    print(osrc,"  :::::::::::updated name")
-
-audio_index.download(src)
-os.rename(ado, upado)
-print("\n Audio Successfully Download \n")
-
-videos[maxx].download(src)
-os.rename(vdo,upvdo)
-print("\n Video Successfully Download \n")
-
-print("Created file Audio :: "+str(upado)+"\n"+
-      "Created file Video :: "+str(upvdo)+"\n"+
-      "Src for A          :: "+str(asrc)+"\n"+
-      "Src for V          :: "+str(vsrc)+"\n"+
-      "Output Path        :: "+str(osrc)+"\n")
-
-
-
-# fname = (f"{video_index.default_filename} vioindex file name")
-# print(src+" scr")
-# print(fname+" fname")
-# print(osrc+" oscr")
-# osrc = Path(f"{src}{fname}")
-
-# print(os.path.isfile(f"D:/vdo from tube/STRUGGLE HARD 😎🔥WhatsApp Status shorts Billionaire Attitude Status🔥motivation quotes.webm"))
-
-# if osrc.is_file(str(fname)):
-#     print("\n  if
-
-print("\n NOT  Exist\n")
-
-# cmd = '-i "ring.webm" -i "rings.webm" -c copy "outpfut.mkv"'
-Title=str(Title)
-os.system(f'ffmpeg -i "{vsrc}" -i "{asrc}" -c copy "{osrc}"')  
-
-
-
-delete(asrc,vsrc)
-
-
+else:
+  print("You're Ready to Go")
 
--- path.py
+++ path.py
@@ -7,3 +7,21 @@ for link in youtube:
 for video in youtube.videos:
     name = video.title
     print(name)
+    
+# import re
+ 
+# # Example input string
+# string = "[~How! @are# $you% ^doing& *today\(). _I- +hope=| <everything> `is~ {fine}]"
+ 
+# # Only keep:
+# # Letters a-z, A-Z
+# # Dots (.)
+# # Numbers (\d)
+# # Non white space (\s)
+# # ^ means other than the provided patterns
+# # need to be substituted by an empty string
+# string = re.sub('[^a-zA-Z.\d\s]', '', string)
+ 
+# # Print the final string. This should print:
+# # How are you doing today. I hope everything is fine
+# print(string)
